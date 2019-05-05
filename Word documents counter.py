f6e32780c755176fabbad332fc5759e6e6ae0614
Matteo Montanari <matteo.montanari13@gmail.com>
2019-05-06 00:20:12 +0200

    Added various extensions to search

--- Word documents counter.py
+++ Word documents counter.py
@@ -1,20 +1,30 @@
 import os
 
 count = 0
+f_types_list = []
+
+f_types = input(
+    "Add the file extensions to find and count (example: .txt .docx .pptx):"
+)
+
+f_types = f_types.split(" ")
+
+for x in f_types:
+    f_types_list.append(x)
 
 path = r"c:" + os.environ["HOMEPATH"] + r"\\Desktop"
 
-print("Scanning the desktop and it's sub-folders to find word documents.")
+print("Scanning the desktop and it's sub-folders to find specified file types.")
 print("Scanning: ")
 
 for d_path, d_names, f_names in os.walk(path):
     for name in f_names:
         _, extension = os.path.splitext(d_path + name)
-        if extension == ".docx" or extension == ".doc" or extension == ".odt":
+        if extension in f_types_list:
             count += 1
             print(name)
 
 print()
-print(f"Done scanning. Found {count} documents")
+print(f"Done scanning. Found {count} files")
 
 input()
