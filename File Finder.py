f774d12d7d2e3c7e435b57e00016246e651b8ca9
sourcedotpy <matteo.montanari13@gmail.com>
2021-03-03 17:57:26 +0100

    Script now searches in every partition

--- File Finder.py
+++ File Finder.py
@@ -1,4 +1,5 @@
 import os
+import psutil
 
 f_types = input(
     "Add the file extensions to find and count (example: .txt .docx .pptx):"
@@ -7,21 +8,22 @@ f_types = input(
 f_types = f_types.split(" ")
 
 # Get partitions and periferals
+partitions = []
+for partition in psutil.disk_partitions():
+    partitions.append(partition.__getattribute__('mountpoint'))
+# print(partitions)
 
+for mount in partitions:
+    path = mount[:-1] + os.environ["HOMEPATH"] + r"\Desktop" if mount == 'C:\\' else mount
 
-path = r"c:" + os.environ["HOMEPATH"] + r"\\Desktop"
-
-print("Scanning the desktop and it's sub-folders to find specified file types.")
-
-count = 0
-for d_path, d_names, f_names in os.walk(path):
-    for name in f_names:
-        _, extension = os.path.splitext(d_path + name)
-        if extension in f_types:
-            count += 1
-            print(name)
+    count = 0
+    for d_path, d_names, f_names in os.walk(path):
+        for name in f_names:
+            _, extension = os.path.splitext(d_path + name)
+            if extension in f_types:
+                count += 1
+                print(d_path + '\\' + name)
 
 print()
 print(f"Done scanning. Found {count} files")
 
-input()
